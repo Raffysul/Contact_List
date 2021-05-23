69cf22a26bdf65a5699ea1377004355f6e406f07
Rafiat <raffysul1@gmail.com>
2021-05-23 23:47:30 +0100

    first commit

--- app/src/main/java/com/example/contact_list/ContactAdapter.kt
+++ app/src/main/java/com/example/contact_list/ContactAdapter.kt
@@ -5,30 +5,30 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contact_list.databinding.ContactListItemBinding
 
-class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
+class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
 
     private val contacts = mutableListOf<Contact>()
 
+    inner class ViewHolder(private val binding: ContactItemLayoutBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(contact: Contact){
+            binding.name.text = contact.name
+            binding.number.text = contact.number
+        }
+    }
+
     fun setupContacts(contact: List<Contact>){
-        this.contacts.addAll(contacts)
+        contacts.addAll(contact)
 
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.ContactViewHolder {
-        return ContactViewHolder(
-            ContactListItemBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-
-            )
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ContactItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
-    override fun onBindViewHolder(holder: ContactAdapter.ContactViewHolder, position: Int) {
-        val contact : Contact = contacts[position]
-        holder.bindItem(contact)
+    override fun onBindViewHolder(holder:ViewHolder, position: Int) {
+        val contact = contacts[position]
+        holder.bind(contact)
 
     }
 
@@ -36,11 +36,6 @@ class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
        return contacts.size
     }
 
-    inner class ContactViewHolder(private val binding: ContactListItemBinding): RecyclerView.ViewHolder(binding.root){
-        fun bindItem(contact: Contact){
-            binding.name.text = contact.name
-            binding.number.text = contact.number
-        }
-    }
+
 
 }
